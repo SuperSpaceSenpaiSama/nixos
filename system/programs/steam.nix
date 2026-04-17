7fa5a205acf6ec552d0461baf499ed792603acd1
Christopher Knight <git@ccox.dev>
2026-04-16 23:32:27 -0400

    Rename packages to remove "xorg".

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -17,9 +17,9 @@
           wget
           pkgsi686Linux.libpulseaudio
           pkgsi686Linux.freetype
-          pkgsi686Linux.xorg.libXcursor
-          pkgsi686Linux.xorg.libXcomposite
-          pkgsi686Linux.xorg.libXi
+          pkgsi686Linux.libXcursor
+          pkgsi686Linux.libXcomposite
+          pkgsi686Linux.libXi
         ];
       };
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
