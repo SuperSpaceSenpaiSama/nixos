fa4854c91c62eab467d055af8cb1a8fdcc2efa38
Christopher Knight <git@ccox.dev>
2026-06-01 14:27:36 -0400

    Update public SSH key.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  local = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF4xbd0SvaUGdrLcMQJkqdxCAWa6qirD0KTFTL6zFAhV";
+  local = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAIF13DzjHLHgjybbCH+twWjMOQjeFdFew8PhhAPIW91";
 in
 {
   "kagi.age".publicKeys = [ local ];
