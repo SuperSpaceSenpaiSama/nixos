23469a1151e8027bbfeeb365e64191a28781e232
Christopher Knight <git@ccox.dev>
2026-06-01 14:21:04 -0400

    Updated formatting of keybinds.

--- home/niri/keybinds.nix
+++ home/niri/keybinds.nix
@@ -18,60 +18,64 @@ let
 in
 {
   programs.niri.settings.binds = with config.lib.niri.actions; {
+    # Prevent programs from blocking key presses
+    "Mod+Escape".action.toggle-keyboard-shortcuts-inhibit = [ ];
+
     # Volume
     "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase"; # output increase
     "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease"; # output decrease
     "XF86AudioMute".action.spawn = noctalia "volume muteOutput"; # output mute
-    "shift+XF86AudioRaiseVolume".action.spawn = noctalia "volume increaseInput"; # input increase
-    "shift+XF86AudioLowerVolume".action.spawn = noctalia "volume decreaseInput"; # input decrease
-    "shift+XF86AudioMute".action.spawn = noctalia "volume muteInput"; # input mute
-    "control+XF86AudioMute".action.spawn = noctalia "volume togglePanel"; # open volume panel
+    "Shift+XF86AudioRaiseVolume".action.spawn = noctalia "volume increaseInput"; # input increase
+    "Shift+XF86AudioLowerVolume".action.spawn = noctalia "volume decreaseInput"; # input decrease
+    "Shift+XF86AudioMute".action.spawn = noctalia "volume muteInput"; # input mute
+    "Mod+W".action.spawn = noctalia "volume muteInput"; # input mute alternate
+    "Control+XF86AudioMute".action.spawn = noctalia "volume togglePanel"; # open volume panel
 
     # Media
     "XF86AudioPlay".action.spawn = noctalia "media playPause";
     "XF86AudioNext".action.spawn = noctalia "media next";
     "XF86AudioPrev".action.spawn = noctalia "media previous";
 
-    "super+Space".action.spawn = noctalia "launcher toggle";
-    "super+q".action = close-window;
-    "super+b".action = spawn apps.browser;
-    "super+Return".action = spawn apps.terminal;
-    #    "super+Space".action = spawn apps.appLauncher;
-    "super+E".action = spawn apps.fileManager;
-    "super+L".action.spawn = noctalia "lockScreen lock";
+    "Mod+Space".action.spawn = noctalia "launcher toggle";
+    "Mod+Q".action = close-window;
+    "Mod+B".action = spawn apps.browser;
+    "Mod+Return".action = spawn apps.terminal;
+    #    "Mod+Space".action = spawn apps.appLauncher;
+    "Mod+E".action = spawn apps.fileManager;
+    "Mod+L".action.spawn = noctalia "lockScreen lock";
 
     # Tested with ghostty and kitty
-    # "super+m".action = spawn apps.terminal [
+    # "Mod+M".action = spawn apps.terminal [
     #   "--title=spotify_player"
     #   "-e"
     #   "spotify_player"
     # ];
 
     # 1Passord quick access
-    "super+p".action = spawn [
+    "Mod+p".action = spawn [
       "${pkgs._1password-gui}/bin/1password"
       "--quick-access"
     ];
 
-    "super+f".action = fullscreen-window;
-    "super+t".action = toggle-window-floating;
+    "Mod+F".action = fullscreen-window;
+    "Mod+T".action = toggle-window-floating;
 
-    "control+shift+1".action.screenshot = [ ];
-    "control+shift+2".action.screenshot-window = [ ];
+    "Control+Shift+1".action.screenshot = [ ];
+    "Control+Shift+2".action.screenshot-window = [ ];
 
-    "super+Left".action = focus-column-left;
-    "super+Right".action = focus-column-right;
-    "super+Down".action = focus-workspace-down;
-    "super+Up".action = focus-workspace-up;
+    "Mod+Left".action = focus-column-left;
+    "Mod+Right".action = focus-column-right;
+    "Mod+Down".action = focus-workspace-down;
+    "Mod+Up".action = focus-workspace-up;
 
-    "super+Shift+Left".action = move-column-left;
-    "super+Shift+Right".action = move-column-right;
-    "super+Shift+Down".action = move-column-to-workspace-down;
-    "super+Shift+Up".action = move-column-to-workspace-up;
+    "Mod+Shift+Left".action = move-column-left;
+    "Mod+Shift+Right".action = move-column-right;
+    "Mod+Shift+Down".action = move-column-to-workspace-down;
+    "Mod+Shift+Up".action = move-column-to-workspace-up;
 
-    "super+1".action = focus-workspace "main";
-    "super+2".action = focus-workspace "browser";
-    "super+3".action = focus-workspace "discord";
-    "super+4".action = focus-workspace "music";
+    "Mod+1".action = focus-workspace "main";
+    "Mod+2".action = focus-workspace "browser";
+    "Mod+3".action = focus-workspace "discord";
+    "Mod+4".action = focus-workspace "music";
   };
 }
